922942bc2c9bee8d850c9cdc15f8d5d6f62d7bf1
fidelis182 <fedymueni18@gmail.com>
2023-07-28 06:37:38 -0700

    prints env to output

---
+++ custom_env.c
@@ -0,0 +1,19 @@
+#include "simple_shell.h"
+
+/**
+* printenv - prints the environment string to stdout
+*
+* Return: 0
+*/
+void printenv(void)
+{
+	int i = 0;
+	char **env = environ;
+
+	while (env[i])
+	{
+		write(STDOUT_FILENO, (const void *)env[i], _strlen(env[xi));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+}
